154da0fcadbb5bd40f7dd330e4b4940366e32035
sundarcodes <sundar82@gmail.com>
2015-11-18 12:02:50 +0530

    Added functionality for landing page after user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    home_land_path
+  end
 
 protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/land'
+
   get 'welcome/index'
 
   devise_for :users
